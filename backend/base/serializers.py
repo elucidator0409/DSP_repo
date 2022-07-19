07853a436efb6c9b6edd8d693ef25706c2f1b8ad
Elucidator0409 <tranquockhanhdhsp99@gmail.com>
2022-07-19 02:47:44 +0100

    Create order, bug at shipping address

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -65,7 +65,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_shippingAddress(self, obj):
         try:
-            address = ShippingAddressSerializer(obj.shippingaddress, many=False).data
+            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
         except:
             address = False    
         return address
